07e3a87c1620f2ac884aa3f11441154ae4bfce66
joeyoungmin <quswmf45@naver.com>
2024-11-18 16:13:54 +0900

    js

--- client/chapter/dom/07.event.js
+++ client/chapter/dom/07.event.js
@@ -28,20 +28,70 @@ about.addEventListener('wheel', handler);
 const ground = getNode('.ground');
 const ball = getNode('.ball');
 
+function handleBall({ offsetX: x, offsetY: y }) {
+  // const { offsetX:x, offsetY:y } = e;
+
+  // const x = e.offsetX
+  // const y = e.offsetY
+
+  const w = ball.offsetWidth;
+  const h = ball.offsetHeight;
+
+  ball.style.transform = `translate(${x - w / 2}px,${y - h / 2}px)`;
+}
+
+ground.addEventListener('click', handleBall);
+
 function handleMove({ offsetX: x, offsetY: y }) {
   // const w = ball.offsetWidth;
   // const h = ball.offsetHeight;
   // ball.style.transform = `translate(${x - (w / 2)}px,${y - (h / 2)}px)`
 
   const template = `
-      <div class="emotion" style="top:${y}px; left:${x}px">💩</div>
-    `;
+    <div class="emotion" style="top:${y}px; left:${x}px">🥰</div>
+  `;
   insertLast(ground, template);
 }
 
-ground.addEventListener('mousemove', handleMove);
+// debounce
+
+window.addEventListener('resize', () => {
+  console.log('값 계산중...'); // 1s
+});
+
+function debounce(callback, limit = 500) {
+  let timeout;
+
+  return function (e) {
+    clearTimeout(timeout);
+
+    timeout = setTimeout(() => {
+      callback.call(this, e);
+    }, limit);
+  };
+}
+
+function handle(e) {
+  console.log(e);
+}
+
+ground.addEventListener('mousemove', throttle(handle, 1000));
+
+function throttle(callback, limit = 500) {
+  let wait = false;
+
+  return function (...args) {
+    if (!wait) {
+      callback.apply(this, args);
+      wait = true;
+      setTimeout(() => (wait = false), limit);
+    }
+  };
+}
+
+// throttle
 
 /* 이벤트 추가/제거 --------------------------------------------------------- */
 
 // - addEventListener
-// - removeEventListene
+// - removeEventListener
---
+++ client/chapter/dom/08.bubble.js
@@ -0,0 +1,7 @@
+const section = getNode('section');
+const article = getNode('article');
+const p = getNode('p');
+
+section.addEventListener('click', () => {
+  console.log('section');
+});
