e96cfa90d077a8435042e7cf4ae0b76e7cd222d1
kindtiger <seonbeom2@gmail.com>
2024-11-19 14:02:39 +0900

    learn. delegation
    - SNS
       - infinity scrolling

--- client/js/part04.js
+++ client/js/part04.js
@@ -190,7 +190,8 @@ function handleInfinityScroll(){
   const documentHeight = document.documentElement.scrollHeight;
   
   if(scrollTop + viewportHeight >= documentHeight - 150){
-    console.log('event 발생!');
+    
+    getNode('.container').insertAdjacentHTML('beforeend',article);
     
   }
   
