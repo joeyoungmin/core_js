8f3ea47132894f8808aef1e77bea76543664e9ba
joeyoungmin <quswmf45@naver.com>
2024-11-21 15:58:10 +0900

    js

--- client/lib/dom/attr.js
+++ client/lib/dom/attr.js
@@ -1,23 +1,23 @@
+import { isString, isUndefined } from '../utils/type.js';
+import { getNode } from './getNode.js';
+import { typeError } from '../error/typeError.js';
+
 function getAttr(node, prop) {
   if (isString(node)) node = getNode(node);
-  // if(typeof node === 'string') node = document.querySelector(node);
 
   if (!isString(prop))
-    throw new TypeError(
-      'getAttr 함수의 두 번째 인수는 문자 타입 이어야 합니다.'
+    throw typeError(
+      'getAttr 함수에 전달된 두 번째 인수는 문자 타입 이어야 합니다.'
     );
 
   return node.getAttribute(prop);
 }
 
-function typeError(message) {
-  return new TypeError(message + '문자 타입 이어야 합니다.');
-}
-
 function setAttr(node, prop, value) {
   if (isString(node)) node = getNode(node);
-
-  if (value === '') {
+  if (!isString(prop))
+    throw typeError('setAttr 함수의 두 번째 인수는 문자 타입 이어야 합니다.');
+  if (isUndefined(value) || value === '') {
     node.removeAttribute(prop);
     return;
   }
@@ -28,15 +28,11 @@ function setAttr(node, prop, value) {
     return;
   }
 
-  if (!value)
-    throw new ReferenceError(
-      'setAttr 함수의 세 번째 인수는 필수 입력값 입니다.'
-    );
-
   node.setAttribute(prop, value);
 }
 
 // function attr(node,prop,value){
+
 //   if(!value){
 //     return getAttr(node,prop)
 //   }else{
@@ -44,5 +40,8 @@ function setAttr(node, prop, value) {
 //   }
 // }
 
-const attr = (node, prop, value) =>
+export const attr = (node, prop, value) =>
   !value ? getAttr(node, prop) : setAttr(node, prop, value);
+
+// attr('.about','id') // getter
+// attr('.about','id','star') // setter
