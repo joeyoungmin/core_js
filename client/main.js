3f2e175c5a504ddbd81b77b18193e61806bd17cf
joeyoungmin <quswmf45@naver.com>
2024-11-26 14:45:43 +0900

    component

--- client/main.js
+++ client/main.js
@@ -1,39 +1,51 @@
-import { getNode, setStorage, getStorage, deleteStorage } from './lib/index.js';
+// console.log(temp.content.cloneNode(true));
 
-const textField = getNode('#textField');
-const clearButton = getNode('button[data-name="clear"]');
+class MyElement extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+    this.count = 0;
+  }
 
-// 1. 인풋 이벤트 바인딩
-//     - 인풋(textarea) 태그 선택
-//     - addEventListener('input',handler)
-//     - handler 함수 안에서 값 가져오기 (this.value)
+  connectedCallback() {
+    this.랜더링();
+    this.card = this.shadowRoot.querySelector('.card');
+    this.card.addEventListener('click', () => {
+      this.이벤트();
+    });
+    this.card.addEventListener('click', this.이벤트.bind(this));
+  }
 
-// 2. 인풋 값을 로컬 스토리지에 저장(타이핑 하는 순간 순간마다)
-//     - setStorage(key,value)
+  이벤트() {
+    console.log(++this.count);
+  }
 
-// 3. init 함수 안에서 로컬스토리지에 있는 값을 가져와 인풋의 value로 설정
-//     - getStorage
-//     - text.value = value
+  랜더링() {
+    this.shadowRoot.innerHTML = `
+      <style>
+        .card{
+          background-color:orange;
+        }
+      </style>
 
-// 4. 새로고침 => 데이터 유지
+      <div class="card">
+        <h2>Card Title</h2>
+        <slot name="me"></slot>
+        <p> description..</p>
+      </div>
+    `;
+  }
 
-// 5. clear 버튼 클릭시 데이터 제거 (로컬스토리지, 인풋값)
+  static get observedAttributes() {
+    return ['data-name'];
+  }
 
-function handleInput() {
-  const value = this.value;
-  setStorage('text', value);
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name === 'data-name') {
+      this.랜더링();
+      console.log('리랜더링');
+    }
+  }
 }
 
-function handleClear() {
-  textField.value = '';
-  deleteStorage('text');
-}
-
-function init() {
-  getStorage('text').then((res) => (textField.value = res));
-}
-
-textField.addEventListener('input', handleInput);
-clearButton.addEventListener('click', handleClear);
-
-init();
+customElements.define('my-element', MyElement);
