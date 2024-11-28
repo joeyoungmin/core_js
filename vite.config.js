dbcb3315c1d8dc5c824447875dbd8305482ae023
joeyoungmin <quswmf45@naver.com>
2024-11-28 16:18:24 +0900

    js

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,27 @@
-import { defineConfig } from "vite";
+import {defineConfig} from 'vite';
+import { resolve } from "node:path";
 
-const env  = process.env.NODE.ENV
+
+const env = process.env.NODE_ENV
+
 export default defineConfig({
-    build:{
-        outDir:'docs'
-    },
-    server:{
-        host:"localhost",
-        port:3000,
-        cors:true,
-    },
-    css:{
-        devSourcemap:true,
-        modules:{
-            generateScopedName:env === 'development'? "[local]__[hash:base64:3]" : "[name]__[local]__[hash:base64:5]"
-        },
-    },
-    resolve: {
-        alias:{'@':resolve(__dirname,'src')}
-   }
+  
+
+  build:{
+    outDir: 'docs',
+  },
+  server:{
+    host:'localhost',
+    port:3000,
+    cors:true
+  },
+  css:{
+    devSourcemap:true,
+    modules:{
+      generateScopedName: env === 'development' ? "[local]__[hash:base64:3]" : "[name]__[local]__[hash:base64:5]"
+    }
+  },
+  resolve: {
+    alias:{'@':resolve(__dirname,'src')}
+  }
 })
