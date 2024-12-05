66557555a9e0bacd6a63b5648e7b41ae2534e6a3
joeyoungmin <quswmf45@naver.com>
2024-12-05 09:29:42 +0900

    typescript

---
+++ src/ts/1-1.practice.ts
@@ -0,0 +1,50 @@
+
+
+
+/* primitive type */
+
+
+
+
+// 문자
+let nickName:string = 'tiger';
+
+// 숫자
+let age:number = 35;
+
+// 불린
+let isStudent:boolean = false;
+
+// null
+let nothing:null = null;
+
+// undefined
+let dotDefined:undefined = undefined;
+
+
+console.log(isStudent);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
